ec179022331dbef49fe9b2d6a032ce8b5f29a8ef
Philip Bankosegger <e12122394@student.tuwien.ac.at>
2022-07-20 01:19:42 +0200

    new

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.getElementById("toggle").addEventListener("click", function () {
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
 });
+
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
